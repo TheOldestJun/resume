455b272a6e27af70dc16528725b4c93a60d1487a
Stanislav <s.v.pukha@gmail.com>
2022-12-09 08:50:19 +0200

    minor skills changes

--- components/Skills.component.jsx
+++ components/Skills.component.jsx
@@ -2,63 +2,72 @@ import Image from "next/image";
 import Skill from "./Skill.component";
 
 const Skills = ({ skills, skillsProf }) => {
-  return (
-    <div className=' mt-2 border-2 text-white backdrop-blur-sm md:mt-0'>
-      <div className='container py-2'>
-        <div className='flex gap-2 text-lg font-bold'>
-          <Image
-            src='/briefcase-solid.svg'
-            width={20}
-            height={20}
-            alt='professional skills'
-          />
-          {skills}
+    return (
+        <div className=' mt-2 border-2 text-white backdrop-blur-sm md:mt-0'>
+            <div className='container py-2'>
+                <div className='flex gap-2 text-lg font-bold'>
+                    <Image
+                        src='/briefcase-solid.svg'
+                        width={20}
+                        height={20}
+                        alt='professional skills'
+                    />
+                    {skills}
+                </div>
+                <hr />
+                <Skill
+                    title='English'
+                    progress='80'
+                    prTitle={skillsProf["upperIntermediate"]}
+                />
+                <Skill
+                    title='Google/Stack Overflow'
+                    progress='90'
+                    prTitle={skillsProf["master"]}
+                />
+                <Skill
+                    title='HTML'
+                    progress='80'
+                    prTitle={skillsProf["advanced"]}
+                />
+                <Skill
+                    title='CSS3, Tailwind CSS, SCSS'
+                    progress='50'
+                    prTitle={skillsProf["intermediate"]}
+                />
+                <Skill
+                    title='Javascript ES8'
+                    progress='90'
+                    prTitle={skillsProf["advanced"]}
+                />
+                <Skill
+                    title='Typescript'
+                    progress='20'
+                    prTitle={skillsProf["elementary"]}
+                />
+                <Skill
+                    title='Node.JS / Express.JS'
+                    progress='20'
+                    prTitle={skillsProf["elementary"]}
+                />
+                <Skill
+                    title='React.JS/Redux Toolkit'
+                    progress='50'
+                    prTitle={skillsProf["intermediate"]}
+                />
+                <Skill
+                    title='Next.JS/SSR/CSR'
+                    progress='50'
+                    prTitle={skillsProf["intermediate"]}
+                />
+                <Skill
+                    title='PostgreSQL via Prisma'
+                    progress='20'
+                    prTitle={skillsProf["elementary"]}
+                />
+            </div>
         </div>
-        <hr />
-        <Skill
-          title='English'
-          progress='80'
-          prTitle={skillsProf["upperIntermediate"]}
-        />
-        <Skill title='HTML' progress='90' prTitle={skillsProf["advanced"]} />
-        <Skill
-          title='CSS3, Tailwind CSS, SCSS'
-          progress='50'
-          prTitle={skillsProf["intermediate"]}
-        />
-        <Skill
-          title='Javascript ES8'
-          progress='90'
-          prTitle={skillsProf["advanced"]}
-        />
-        <Skill
-          title='Typescript'
-          progress='30'
-          prTitle={skillsProf["elementary"]}
-        />
-        <Skill
-          title='Node.JS / Express.JS'
-          progress='30'
-          prTitle={skillsProf["elementary"]}
-        />
-        <Skill
-          title='React.JS/Redux Toolkit'
-          progress='50'
-          prTitle={skillsProf["intermediate"]}
-        />
-        <Skill
-          title='Next.JS/SSR/CSR'
-          progress='50'
-          prTitle={skillsProf["intermediate"]}
-        />
-        <Skill
-          title='PostgreSQL via Prisma'
-          progress='30'
-          prTitle={skillsProf["elementary"]}
-        />
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Skills;
