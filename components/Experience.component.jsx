c5cecbb7a97fe224256bc6c3c6117f2ba7f289c9
Stanislav <s.v.pukha@gmail.com>
2023-02-22 14:53:41 +0200

    flex and gap errors fixed

--- components/Experience.component.jsx
+++ components/Experience.component.jsx
@@ -1,38 +1,38 @@
 import Image from "next/image";
 
 const Experience = ({ experience, experienceTable }) => {
-  return (
-    <div className='text-white border-2 border-white py-2 backdrop-blur-sm px-2 my-2'>
-      <div className='text-lg font-bold flex gap-2'>
-        <Image
-          src='/person-digging-solid.svg'
-          width={20}
-          height={20}
-          alt='Experience'
-        />
-        {experience}
-      </div>
-      <hr />
-      <div className='text-md'>
-        <div className='eduTable'>
-          <div className='container flex justify-between my-3'>
-            {experienceTable.webdev}
-            <p>2020-...</p>
-          </div>
-          <p className='mx-9'>{experienceTable.webdevDesc}</p>
-        </div>
-        <a href='https://goo.gl/maps/ZobeiUi8f2W98VwS9' target='blank'>
-          <div className='eduTable'>
-            <div className='container flex justify-between my-3'>
-              {experienceTable.scm}
-              <p>2010-...</p>
+    return (
+        <div className='border-2 border-white py-2 px-2 text-white backdrop-blur-sm'>
+            <div className='flex gap-2 text-lg font-bold'>
+                <Image
+                    src='/person-digging-solid.svg'
+                    width={20}
+                    height={20}
+                    alt='Experience'
+                />
+                {experience}
             </div>
-            <p className='mx-9'>{experienceTable.scmDesc}</p>
-          </div>
-        </a>
-      </div>
-    </div>
-  );
+            <hr />
+            <div className='text-md'>
+                <div className='eduTable'>
+                    <div className='container my-3 flex justify-between'>
+                        {experienceTable.webdev}
+                        <p>2020-...</p>
+                    </div>
+                    <p className='mx-9'>{experienceTable.webdevDesc}</p>
+                </div>
+                <a href='https://goo.gl/maps/ZobeiUi8f2W98VwS9' target='blank'>
+                    <div className='eduTable'>
+                        <div className='container my-3 flex justify-between'>
+                            {experienceTable.scm}
+                            <p>2010-...</p>
+                        </div>
+                        <p className='mx-9'>{experienceTable.scmDesc}</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    );
 };
 
 export default Experience;
--- pages/index.js
+++ pages/index.js
@@ -1,95 +1,97 @@
 import Image from "next/image";
 import {
-  Title,
-  Header,
-  About,
-  Education,
-  Contacts,
-  Experience,
-  Skills,
+    Title,
+    Header,
+    About,
+    Education,
+    Contacts,
+    Experience,
+    Skills,
 } from "../components/";
 import { useSelector } from "react-redux";
 import { lang } from "../constants/localization";
 
 export default function Home() {
-  const local = useSelector((state) => state.lang.lang.payload);
-  return (
-    <>
-      <Image
-        src='/pexels.jpg'
-        width={1200}
-        height={700}
-        alt='background'
-        style={{
-          position: "fixed",
-          zIndex: "-1",
-          top: "0",
-          left: "0",
-          minWidth: "100%",
-          minHeight: "100%",
-          opacity: "90%",
-        }}
-      />
-      <div className='container flex flex-col md:hidden'>
-        <Header welcome={lang[local]["welcome"]} />
-        <main className='container'></main>
-        <Title
-          className='mx-5'
-          name={lang[local]["name"]}
-          title={lang[local]["title"]}
-        />
-        <div className='flex gap-2 py-2'>
-          <div className='w-2/3'>
-            <About
-              about={lang[local]["about"]}
-              aboutText={lang[local]["aboutText"]}
+    const local = useSelector((state) => state.lang.lang.payload);
+    return (
+        <>
+            <Image
+                src='/pexels.jpg'
+                width={1200}
+                height={700}
+                alt='background'
+                style={{
+                    position: "fixed",
+                    zIndex: "-1",
+                    top: "0",
+                    left: "0",
+                    minWidth: "100%",
+                    minHeight: "100%",
+                    opacity: "90%",
+                }}
             />
-            <Education
-              education={lang[local]["education"]}
-              educationTable={lang[local]["educationTable"]}
-            />
-            <Experience
-              experience={lang[local]["experience"]}
-              experienceTable={lang[local]["experienceTable"]}
-            />
-          </div>
-          <div className='w-1/3'>
-            <Contacts
-              address={lang[local]["address"]}
-              contacts={lang[local]["contacts"]}
-            />
-            <Skills
-              skills={lang[local]["skills"]}
-              skillsProf={lang[local]["skillsProf"]}
-            />
-          </div>
-        </div>
-      </div>
-      <div className='hidden gap-2 md:container md:flex md:flex-col'>
-        <Header welcome={lang[local]["welcome"]} />
-        <main className='container'></main>
-        <Title name={lang[local]["name"]} title={lang[local]["title"]} />
-        <Contacts
-          address={lang[local]["address"]}
-          contacts={lang[local]["contacts"]}
-        />
-        <Skills
-          skills={lang[local]["skills"]}
-          skillsProf={lang[local]["skillsProf"]}
-        />
-        <About
-          about={lang[local]["about"]}
-          aboutText={lang[local]["aboutText"]}
-        />
-        <Education
-          education={lang[local]["education"]}
-          educationTable={lang[local]["educationTable"]}
-        />
-        <Experience
-          experience={lang[local]["experience"]}
-          experienceTable={lang[local]["experienceTable"]}
-        />
-      </div>
-    </>
-  );
+            <div className='container flex flex-col md:hidden'>
+                <Header welcome={lang[local]["welcome"]} />
+                <Title
+                    className='mx-5'
+                    name={lang[local]["name"]}
+                    title={lang[local]["title"]}
+                />
+                <div className='flex gap-2 py-2'>
+                    <div className='flex w-2/3 flex-col justify-end'>
+                        <About
+                            about={lang[local]["about"]}
+                            aboutText={lang[local]["aboutText"]}
+                        />
+                        <Education
+                            education={lang[local]["education"]}
+                            educationTable={lang[local]["educationTable"]}
+                        />
+                        <Experience
+                            experience={lang[local]["experience"]}
+                            experienceTable={lang[local]["experienceTable"]}
+                        />
+                    </div>
+                    <div className='w-1/3'>
+                        <Contacts
+                            address={lang[local]["address"]}
+                            contacts={lang[local]["contacts"]}
+                        />
+                        <Skills
+                            skills={lang[local]["skills"]}
+                            skillsProf={lang[local]["skillsProf"]}
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className='hidden gap-2 md:container md:flex md:flex-col'>
+                <Header welcome={lang[local]["welcome"]} />
+                <main className='container'></main>
+                <Title
+                    name={lang[local]["name"]}
+                    title={lang[local]["title"]}
+                />
+                <Contacts
+                    address={lang[local]["address"]}
+                    contacts={lang[local]["contacts"]}
+                />
+                <Skills
+                    skills={lang[local]["skills"]}
+                    skillsProf={lang[local]["skillsProf"]}
+                />
+                <About
+                    about={lang[local]["about"]}
+                    aboutText={lang[local]["aboutText"]}
+                />
+                <Education
+                    education={lang[local]["education"]}
+                    educationTable={lang[local]["educationTable"]}
+                />
+                <Experience
+                    experience={lang[local]["experience"]}
+                    experienceTable={lang[local]["experienceTable"]}
+                />
+            </div>
+        </>
+    );
 }
