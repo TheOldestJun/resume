bfffd4ebfcb9269435b2449ad8903ea663c0dcda
Stanislav <s.v.pukha@gmail.com>
2023-02-23 11:42:55 +0200

    english translation errors fix

--- constants/localization.js
+++ constants/localization.js
@@ -1,116 +1,116 @@
 export const lang = {
-  EN: {
-    welcome: "Choose your language",
-    name: "Stanislav Pukha",
-    title: "Javascript Full Stack Developer",
-    about: "ABOUT ME",
-    aboutText:
-      "Absorb new knowledge like a sponge. Have clear mathematical and analytical thinking. Always listen and hear mentors. Attentive to details. Easily solving conflict situations. Have some basic understanding of Kanban and SCRUM methodologies used in Agile teams.",
-    education: "EDUCATION",
-    educationTable: {
-      bachelor: "Bachelor in software engineering",
-      master: "Master in software engineering",
-      university: "National Mining University of Ukraine",
+    EN: {
+        welcome: "Choose your language",
+        name: "Stanislav Pukha",
+        title: "Javascript Full Stack Developer",
+        about: "ABOUT ME",
+        aboutText:
+            "Absorb new knowledge like a sponge. Have clear mathematical and analytical thinking. Always listen and hear mentors. Attentive to details. Easily solving conflict situations. Have some basic understanding of Kanban and SCRUM methodologies used in Agile teams.",
+        education: "EDUCATION",
+        educationTable: {
+            bachelor: "Bachelor of software engineering",
+            master: "Master of software engineering",
+            university: "National Mining University of Ukraine",
+        },
+        experience: "EXPERIENCE",
+        experienceTable: {
+            webdev: "FULL STACK WEB DEVELOPER",
+            webdevDesc:
+                "Pet projects for education purposes only. Don't have any commercial experience. One full project written with no cost for my friends. See Outlay project in my GitHub profile. Nowadays writing Supply Chain Management System for my current job.",
+            scm: "SUPPLY CHAIN MANAGER",
+            scmDesc:
+                "Carrying out a full cycle of supply an enterprise for 300 people. From applications to delivery of goods and materials. Also coworking as an anykey.",
+        },
+        contacts: "CONTACTS",
+        address: "Horishi Plavni, Poltava region, Ukraine",
+        skills: "PROFESSIONAL SKILLS",
+        skillsProf: {
+            beginner: "Beginner",
+            elementary: "Elementary",
+            intermediate: "Intermediate",
+            upperIntermediate: "Upper Intermediate",
+            advanced: "Advanced",
+            master: "Master",
+            native: "Native",
+        },
+        certificates: "SERTIFICATES",
     },
-    experience: "EXPERIENCE",
-    experienceTable: {
-      webdev: "FULL STACK WEB DEVELOPER",
-      webdevDesc:
-        "Pet projects for education purposes only. Don't have any commercial experience. One full project written with no cost for my friends. See Outlay project in my GitHub profile. Nowadays writing Supply Chain Management System for my current job.",
-      scm: "SUPPLY CHAIN MANAGER",
-      scmDesc:
-        "Carrying out a full cycle of supply an enterprise for 300 people. From applications to delivery of goods and materials. Also coworking as an anykey.",
+    //
+    //UA Localization
+    //
+    UA: {
+        welcome: "Оберіть мову спілкування",
+        name: "Станіслав Пуха",
+        title: "Javascript Full Stack розробник",
+        about: "Про мене",
+        aboutText:
+            "Вбираю нові знання як губка. Володію чітким математичним та аналітичним мисленням. Завжди слухаю і чую наставників. Уважний до деталей. Легко вирішую конфліктні ситуації. Маю базове уявлення про методології Kanban та SCRUM, які використовуються в командах Agile.",
+        education: "Освіта",
+        educationTable: {
+            bachelor: "Бакалавр Інженер-програміст",
+            master: "Спеціаліст Інженер-програміст",
+            university: "Національний Гірничий Університет України",
+        },
+        experience: "Досвід роботи",
+        experienceTable: {
+            webdev: "FULL STACK WEB розробник",
+            webdevDesc:
+                "Роблю пет проекти лише для освітніх цілей. Не маю комерційного досвіду. Один повний проект, написаний безкоштовно для моїх друзів. Перегляньте проект Outlay у моєму профілі GitHub. Зараз пишу систему управління ланцюгами поставок для моєї поточної роботи.",
+            scm: "Інженер з постачання та збуту",
+            scmDesc:
+                "Здійснюю повний цикл постачання підприємства на 300 осіб. Від заявок до доставки ТМЦ. Також підробляю як anykey.",
+        },
+        contacts: "Контакти",
+        address: "Горішні Плавні, Полтавська обл., Украйна",
+        skills: "Профессійні навички",
+        skillsProf: {
+            beginner: "Початковий",
+            elementary: "Eлементарний",
+            intermediate: "Середній",
+            upperIntermediate: "Достатній",
+            advanced: "Досвідчений",
+            master: "Професіональний",
+            native: "Рідний",
+        },
+        certificates: "Сертифікати",
     },
-    contacts: "CONTACTS",
-    address: "Horishi Plavni, Poltava region, Ukraine",
-    skills: "PROFESSIONAL SKILLS",
-    skillsProf: {
-      beginner: "Beginner",
-      elementary: "Elementary",
-      intermediate: "Intermediate",
-      upperIntermediate: "Upper Intermediate",
-      advanced: "Advanced",
-      master: "Master",
-      native: "Native",
+    //
+    //RU Localization
+    //
+    RU: {
+        welcome: "Выберите язык общения",
+        name: "Станислав Пуха",
+        title: "Javascript Full Stack разработчик",
+        about: "Обо мне",
+        aboutText:
+            "Впитываю новые знания как губка. Имею ясное математическое и аналитическое мышление. Всегда слушаю и главное слышу наставников. Внимательный к деталям. Легко решаю конфликтные ситуации. Имею базовое представление о методологиях Kanban и SCRUM, используемых в Agile-командах.",
+        education: "Образование",
+        educationTable: {
+            bachelor: "Бакалавр Инженер-программист",
+            master: "Специалист Инженер-программист",
+            university: "Национальный Горный Университет Украины",
+        },
+        experience: "Опыт работы",
+        experienceTable: {
+            webdev: "FULL STACK WEB разработчик",
+            webdevDesc:
+                "Пет проекты только для образовательных целей. Полноценного коммерческого опыта у меня нет. Один полный проект, написанный бесплатно для моих друзей. Смотрите проект Outlay в моем профиле GitHub. В настоящее время пишу систему управления цепочками поставок для моей текущей работы.",
+            scm: "Инженер по снабжению и сбыту",
+            scmDesc:
+                "Осуществляю полный цикл снабжения предприятия на 300 человек. От заявок до доставки ТМЦ. Также подрабатываю как anykey.",
+        },
+        contacts: "Контакты",
+        address: "Горишние Плавни, Полтавская обл., Украина",
+        skills: "Профессиональные навыки",
+        skillsProf: {
+            beginner: "Начальный",
+            elementary: "Элементарный",
+            intermediate: "Средний",
+            upperIntermediate: "Достаточный",
+            advanced: "Опытный",
+            master: "Профессиональный",
+            native: "Родной",
+        },
+        certificates: "Сертификаты",
     },
-    certificates: "SERTIFICATES",
-  },
-  //
-  //UA Localization
-  //
-  UA: {
-    welcome: "Оберіть мову спілкування",
-    name: "Станіслав Пуха",
-    title: "Javascript Full Stack розробник",
-    about: "Про мене",
-    aboutText:
-      "Вбираю нові знання як губка. Володію чітким математичним та аналітичним мисленням. Завжди слухаю і чую наставників. Уважний до деталей. Легко вирішую конфліктні ситуації. Маю базове уявлення про методології Kanban та SCRUM, які використовуються в командах Agile.",
-    education: "Освіта",
-    educationTable: {
-      bachelor: "Бакалавр Інженер-програміст",
-      master: "Спеціаліст Інженер-програміст",
-      university: "Національний Гірничий Університет України",
-    },
-    experience: "Досвід роботи",
-    experienceTable: {
-      webdev: "FULL STACK WEB розробник",
-      webdevDesc:
-        "Роблю пет проекти лише для освітніх цілей. Не маю комерційного досвіду. Один повний проект, написаний безкоштовно для моїх друзів. Перегляньте проект Outlay у моєму профілі GitHub. Зараз пишу систему управління ланцюгами поставок для моєї поточної роботи.",
-      scm: "Інженер з постачання та збуту",
-      scmDesc:
-        "Здійснюю повний цикл постачання підприємства на 300 осіб. Від заявок до доставки ТМЦ. Також підробляю як anykey.",
-    },
-    contacts: "Контакти",
-    address: "Горішні Плавні, Полтавська обл., Украйна",
-    skills: "Профессійні навички",
-    skillsProf: {
-      beginner: "Початковий",
-      elementary: "Eлементарний",
-      intermediate: "Середній",
-      upperIntermediate: "Достатній",
-      advanced: "Досвідчений",
-      master: "Професіональний",
-      native: "Рідний",
-    },
-    certificates: "Сертифікати",
-  },
-  //
-  //RU Localization
-  //
-  RU: {
-    welcome: "Выберите язык общения",
-    name: "Станислав Пуха",
-    title: "Javascript Full Stack разработчик",
-    about: "Обо мне",
-    aboutText:
-      "Впитываю новые знания как губка. Имею ясное математическое и аналитическое мышление. Всегда слушаю и главное слышу наставников. Внимательный к деталям. Легко решаю конфликтные ситуации. Имею базовое представление о методологиях Kanban и SCRUM, используемых в Agile-командах.",
-    education: "Образование",
-    educationTable: {
-      bachelor: "Бакалавр Инженер-программист",
-      master: "Специалист Инженер-программист",
-      university: "Национальный Горный Университет Украины",
-    },
-    experience: "Опыт работы",
-    experienceTable: {
-      webdev: "FULL STACK WEB разработчик",
-      webdevDesc:
-        "Пет проекты только для образовательных целей. Полноценного коммерческого опыта у меня нет. Один полный проект, написанный бесплатно для моих друзей. Смотрите проект Outlay в моем профиле GitHub. В настоящее время пишу систему управления цепочками поставок для моей текущей работы.",
-      scm: "Инженер по снабжению и сбыту",
-      scmDesc:
-        "Осуществляю полный цикл снабжения предприятия на 300 человек. От заявок до доставки ТМЦ. Также подрабатываю как anykey.",
-    },
-    contacts: "Контакты",
-    address: "Горишние Плавни, Полтавская обл., Украина",
-    skills: "Профессиональные навыки",
-    skillsProf: {
-      beginner: "Начальный",
-      elementary: "Элементарный",
-      intermediate: "Средний",
-      upperIntermediate: "Достаточный",
-      advanced: "Опытный",
-      master: "Профессиональный",
-      native: "Родной",
-    },
-    certificates: "Сертификаты",
-  },
 };
